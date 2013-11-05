21aa89bbea92c19ce9295add04b528cb3b915f15
Hannah Ritter <sibylbelle@gmail.com>
2013-11-04 20:34:07 -0500

    added homework, remocved class file

---
+++ files/src/com/example/classlab7/ListFragment.java
@@ -0,0 +1,61 @@
+package com.example.classlab7;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.Button;
+
+public class ListFragment extends Fragment {
+	
+	private OnItemSelectedListener listener;  
+
+	@Override  
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,  
+	Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.list_fragment, container, false); 
+		
+		Button button = (Button) view.findViewById(R.id.button1); 
+		Button button1 = (Button) view.findViewById(R.id.button2);  
+		Button button2 = (Button) view.findViewById(R.id.button3);  
+		Button button3 = (Button) view.findViewById(R.id.button4); 
+		
+		button.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				updateDetail("layout1"); 				
+			}
+		});
+		
+		button1.setOnClickListener(new View.OnClickListener() {  
+			public void onClick(View v) {                 
+			updateDetail("layout1");  
+			}  
+			});  
+			  
+			button2.setOnClickListener(new View.OnClickListener() {  
+			public void onClick(View v) {             
+			updateDetail("layout3");  
+			}  
+			});  
+			
+			button3.setOnClickListener(new View.OnClickListener() {  
+				public void onClick(View v) {             
+				updateDetail("layout4");  
+				}  
+				});  
+		
+		return view; 
+	}
+	
+	public interface OnItemSelectedListener {  
+		public void onRssItemSelected(String link);  
+		}  
+	
+	public void updateDetail(String s) {          
+		listener.onRssItemSelected(s);  
+		}  
+}
